e94ae0ffb3b66cda3d5dd4af517fb9597dc07456
saba kuljanishvili <kuljanisaba@gmail.com>
2025-04-05 17:18:52 +0400

    user list

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent {
 
+  users: any[] = [];
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe((res) => {
+      this.users = res.data;
+    });
+  }
+
+  viewUserDetails(id: number) {
+    this.router.navigate(['/users', id]);
+
+  }
+
+  goToUserDetail(id: number) {
+    this.router.navigate(['/users', id]);
+  }
+  
 }
